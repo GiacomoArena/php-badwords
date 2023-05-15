3cdb9c9ce28da7836d37bc7bbe8da16acae585e8
GiacomoArena <giacomoarena28@gmail.com>
2023-05-15 15:38:40 +0200

    update commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- bootstrap -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+
+  <title>PHP Badwords</title>
+</head>
+<body>
+  <h1>PHP Badwords</h1>
+
+  <div class="container">
+
+  <form>
+  <div class="form-label">
+
+    <label for="text-area">text</label>
+    <textarea class="form-control" placeholder="write your paragraph here"  name='paragraph'id="paragraph" style="height: 100px"></textarea>
+  </div>
+
+  <div class="mb-3">
+    <label for="badwords" class="form-label">Bad word</label>
+    <input type="text" name='bad_word' class="form-control" id="bad_word"
+    placeholder="write the word you want to censor">
+  </div>
+  
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+  </div>
+</body>
+</html>
